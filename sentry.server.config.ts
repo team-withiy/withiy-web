53da8738985e7bd67f9a107cbd0a37900e09bb24
rldnd <gi981226@gmail.com>
2025-03-29 21:07:34 +0900

    feat(env): use env-cmd to resolve env issue on github actions
    Related to: #6

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,7 +1,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_SENTcRY_DSN,
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   environment: process.env.NODE_ENV,
   tracesSampleRate: process.env.NODE_ENV === "production" ? 0.2 : 1.0,
   ignoreErrors: ["Connection pool closed", "Database connection timeout"],
--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,8 +1,12 @@
+import { config } from "dotenv";
+import { resolve } from "path";
 import { afterAll, afterEach, beforeAll } from "vitest";
 
 import { server } from "@/app/mocks/server";
 
 import "@testing-library/jest-dom/vitest";
+
+config({ path: resolve(__dirname, "env/.env.test") });
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
