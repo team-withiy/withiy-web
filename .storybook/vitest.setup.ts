dbaa4df0ead88fe8a1ce02969d37b4072f9c1011
rldnd <gi981226@gmail.com>
2025-03-30 20:29:16 +0900

    feat(story-book): remove storybook test
    Related to: #2

--- .storybook/vitest.setup.ts
+++
@@ -1,8 +0,0 @@
-import { setProjectAnnotations } from "@storybook/experimental-nextjs-vite";
-import { beforeAll } from "vitest";
-
-import * as projectAnnotations from "./preview";
-
-const project = setProjectAnnotations([projectAnnotations]);
-
-beforeAll(project.beforeAll);
--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -1,5 +1,5 @@
 import { composeStories } from "@storybook/react";
-import { screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { describe, expect, test } from "vitest";
 
 import * as stories from "./Button.stories";
@@ -8,7 +8,7 @@ const { Default } = composeStories(stories);
 
 describe("Button", () => {
   test("should render the button", async () => {
-    await Default.run();
+    render(<Default />);
     expect(screen.getByTestId("this-is-button")).toBeInTheDocument();
   });
 });
