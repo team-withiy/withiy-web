37b1c7ad64386e5b6c20b525f18808060a43a5d4
rldnd <gi981226@gmail.com>
2025-04-08 19:53:45 +0900

    feat(design-system): textarea
    Related to: #12

---
+++ src/shared/ui/Textarea/Textarea.stories.tsx
@@ -0,0 +1,86 @@
+import { ComponentProps } from "react";
+
+import { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+import { useForm } from "react-hook-form";
+
+import Textarea from "./Textarea";
+
+const Template = ({ ...props }: ComponentProps<typeof Textarea>) => {
+  const { register, watch } = useForm<{ value: string }>({
+    defaultValues: {
+      value: "",
+    },
+  });
+
+  return <Textarea {...props} {...register("value")} value={watch("value")} />;
+};
+
+const meta: Meta<typeof Textarea> = {
+  title: "Components/Textarea",
+  parameters: {
+    docs: {
+      description: {
+        component: "Textarea 컴포넌트 입니다.",
+      },
+    },
+  },
+  component: Textarea,
+  argTypes: {
+    placeholder: {
+      description: "Textarea내 입력값이 없을 때 보여지는 텍스트",
+      control: {
+        type: "text",
+      },
+    },
+    disabled: {
+      description: "Textarea 비활성화 여부",
+      table: {
+        type: {
+          summary: "boolean",
+        },
+        defaultValue: { summary: "false" },
+      },
+      control: {
+        type: "boolean",
+      },
+    },
+    rows: {
+      description: "Textarea의 행 수",
+      table: {
+        type: {
+          summary: "number",
+        },
+        defaultValue: { summary: "5" },
+      },
+      control: {
+        type: "number",
+      },
+    },
+    maxLength: {
+      description: "Textarea의 최대 글자 수",
+      table: {
+        type: {
+          summary: "number",
+        },
+      },
+      control: {
+        type: "number",
+      },
+    },
+  },
+  render: Template,
+  args: {
+    placeholder: "Placeholder",
+    disabled: false,
+    rows: 5,
+    onChange: fn(),
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Textarea>;
+
+export const Default: Story = {
+  args: {},
+};
