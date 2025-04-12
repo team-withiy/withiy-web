65323c5d9326181289ea312f43b4ecfb7761e3be
rldnd <gi981226@gmail.com>
2025-04-12 16:52:58 +0900

    feat(design-system): checkbox
    Related to: #12

---
+++ public/icons/check/index.ts
@@ -0,0 +1,2 @@
+export { default as IconCheckLarge } from "./check-large.svg";
+export { default as IconCheckMedium } from "./check-medium.svg";
---
+++ src/shared/ui/Checkbox/Checkbox.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import type { ComponentProps } from "react";
+
+import cx from "clsx";
+
+import { IconCheckLarge, IconCheckMedium } from "public/icons/check";
+
+import styles from "./Checkbox.module.scss";
+
+type Size = 24 | 20;
+
+interface Props extends Omit<ComponentProps<"input">, "size"> {
+  size: Size;
+}
+
+const SIZE_MAPPER: Record<Size, string> = {
+  24: styles.large,
+  20: styles.medium,
+};
+
+const Checkbox: React.FC<Props> = ({ size, children, ...props }) => {
+  return (
+    <label className={cx(styles.wrapper, SIZE_MAPPER[size])} data-testid="checkbox-wrapper">
+      <input type="checkbox" className={styles.checkbox} data-testid="checkbox-input" {...props} hidden />
+      <div className={styles.box}>
+        {size === 24 && <IconCheckLarge className={styles.check} />}
+        {size === 20 && <IconCheckMedium className={styles.check} />}
+      </div>
+      {children}
+    </label>
+  );
+};
+
+export default Checkbox;
