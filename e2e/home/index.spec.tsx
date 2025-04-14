f44dd15f5ee3f2cc5bb79f82c91d9fdd41424989
rldnd <gi981226@gmail.com>
2025-04-14 14:23:05 +0900

    ci(dockerfile): segregate env test & e2e, node version 20 to 22
    Related to: #14

--- e2e/home/index.spec.tsx
+++ e2e/home/index.spec.tsx
@@ -3,7 +3,7 @@ import { expect, test } from "@playwright/test";
 test.describe("HomePage", () => {
   test("should render the home page", async ({ page }) => {
     await page.goto("/");
-    const mswData = await page.getByTestId("mswData").innerText();
-    expect(mswData).toBe('{"data":{"name":"rldnd","age":28}}');
+    const title = page.locator("h1");
+    await expect(title).toHaveText("Home");
   });
 });
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -2,13 +2,9 @@ import Test from "./Test";
 import { IconFile } from "public/icons";
 
 export default async function Home() {
-  const data = await fetch("https://api.example.com/api/user");
-  const json = await data.json();
-
   return (
     <div style={{ backgroundColor: "black" }}>
       <h1>Home</h1>
-      <p data-testid="mswData">{JSON.stringify(json)}</p>
       <Test />
       <IconFile data-testid="svg" />
     </div>
