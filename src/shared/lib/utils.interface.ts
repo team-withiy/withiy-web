e12f129d90d442d129a0579e9d60b5815441241a
rldnd <gi981226@gmail.com>
2025-05-15 18:07:59 +0900

    fix(middleware-refresh): processes to refresh without access token in middleware
    Fixes: #27
    Related to: #26

--- src/shared/lib/utils.interface.ts
+++ src/shared/lib/utils.interface.ts
@@ -1,3 +1,5 @@
 export type MakeKeyRequired<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>;
 
 export type Nullable<T> = T extends object ? { [K in keyof T]: T[K] | null } : T | null;
+
+export type Nilable<T> = T extends object ? { [K in keyof T]: T[K] | null | undefined } : T | null | undefined;
--- src/shared/models/auth/validateToken.ts
+++ src/shared/models/auth/validateToken.ts
@@ -1,9 +1,11 @@
 import { jwtDecode } from "jwt-decode";
 
 import type { TokenDTO } from "@/shared/api/auth/auth.interface";
+import { Nilable } from "@/shared/lib/utils.interface";
 
-export const getTokenExpirationDate = (token: string): Date => {
+export const getTokenExpirationDate = (token?: string | null): Date => {
   try {
+    if (!token) throw new Error("Token is null or undefined");
     const decoded = jwtDecode<{ exp: number }>(token);
     return new Date(decoded.exp * 1000);
   } catch {
@@ -15,8 +17,9 @@ type IsValidToken = {
   [K in keyof TokenDTO as `is${Capitalize<string & K>}Valid`]: boolean;
 };
 
-export const isValidToken = ({ accessToken, refreshToken }: TokenDTO): IsValidToken => {
+export const isValidToken = ({ accessToken, refreshToken }: Nilable<TokenDTO>): IsValidToken => {
   const currentTime = new Date();
+
   return {
     isAccessTokenValid: getTokenExpirationDate(accessToken) > currentTime,
     isRefreshTokenValid: getTokenExpirationDate(refreshToken) > currentTime,
