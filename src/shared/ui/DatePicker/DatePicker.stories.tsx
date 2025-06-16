0c90e7d608ac4597e83795d222dc8259e2e0d1bc
rldnd <gi981226@gmail.com>
2025-06-16 17:20:02 +0900

    feat(calendar-modal): apply datePicker storybook
    Resolves: #38

--- src/shared/ui/DatePicker/DatePicker.stories.tsx
+++ src/shared/ui/DatePicker/DatePicker.stories.tsx
@@ -1,23 +1,54 @@
+import { ComponentProps, useState } from "react";
+
 import { Meta, StoryObj } from "@storybook/react";
 
 import DatePicker from ".";
+import type { SelectedDate } from "./datepicker.interface";
+
+const Template = ({ placeholder }: ComponentProps<typeof DatePicker>) => {
+  const [selectedDate, setSelectedDate] = useState<SelectedDate>();
+  return <DatePicker selectedDate={selectedDate} onDateChange={setSelectedDate} placeholder={placeholder} />;
+};
 
 const meta: Meta<typeof DatePicker> = {
   title: "Components/DatePicker",
   component: DatePicker,
   parameters: {
-    docs: {},
+    docs: {
+      description: {
+        component:
+          "DatePicker 컴포넌트입니다.<br/>초기 조회 형태는 month 이며, 화살표를 누를 경우 year로 변경되고, 화살표를 통해 조회 연도를 변경한 뒤 월 선택을 통해 조회 연월을 변경할 수 있습니다.<br/>선택된 날짜가 없다면 확인 버튼은 비활성화 상태입니다.",
+      },
+      source: {
+        type: "dynamic",
+        language: "tsx",
+        code: `<DatePicker\n  selectedDate={new Date()}\n  onDateChange={() => {}}\n  placeholder="날짜를 선택하세요"\n  className={styles.datePicker}\n/>`,
+      },
+    },
+  },
+  render: Template,
+  argTypes: {
+    selectedDate: {
+      description: "선택된 날짜",
+      table: {
+        type: {
+          summary: "Date | Dayjs | null | undefined",
+        },
+      },
+    },
+    placeholder: {
+      description: "선택된 날짜가 보이지 않을 때 표시되는 텍스트",
+      control: {
+        type: "text",
+      },
+    },
+  },
+  args: {
+    placeholder: "날짜를 선택하세요",
   },
 };
 
 export default meta;
 type Story = StoryObj<typeof DatePicker>;
 
-export const Default: Story = {
-  args: {
-    selectedDate: new Date(),
-    placeholder: "날짜를 선택하세요",
-    className: "custom-class",
-  },
-  render: (args) => <DatePicker {...args} />,
-};
+export const Default: Story = {};
