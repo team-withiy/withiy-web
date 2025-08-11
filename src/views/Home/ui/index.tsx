4eafab0ef027a1dda1d63f7f4ccdbee6b44adaba
rldnd <gi981226@gmail.com>
2025-08-11 18:35:11 +0900

    style(category): remove unused categoryList components

--- src/views/Home/ui/index.tsx
+++ src/views/Home/ui/index.tsx
@@ -1,16 +1,10 @@
-import { Suspense } from "react";
-
-import CategoryList, { LoadingCategoryList } from "@/widgets/CategoryList/ui";
 import BottomNavigation from "@/widgets/Layout/ui/BottomNavigation";
 import GNB from "@/widgets/Layout/ui/GNB";
 
 import RequireAuthorizationWrapper from "@/features/checkAuthorization/ui/RequireAuthorizationWrapper";
 import { withAuthorizationRoute } from "@/features/handleAuthorizationRoute/ui";
 
-import { getCategoriesApi } from "@/entities/category/api/category.server";
 import ScheduleCard from "@/entities/schedule/ui/ScheduleCard";
-
-import FetchBoundary from "@/shared/ui/Boundary/FetchBoundary";
 
 import styles from "./index.module.scss";
 
@@ -27,11 +21,6 @@ async function Home() {
         >
           <ScheduleCard />
         </RequireAuthorizationWrapper>
-        <Suspense fallback={<LoadingCategoryList />}>
-          <FetchBoundary fetchFunctions={[getCategoriesApi]}>
-            {([categories]) => <CategoryList categories={categories.data} />}
-          </FetchBoundary>
-        </Suspense>
       </div>
       <BottomNavigation currentHref="/" />
     </main>
