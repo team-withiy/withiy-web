8e74a20fb839963b94910dd1253a2cee34479feb
rldnd <gi981226@gmail.com>
2025-09-05 23:28:19 +0900

    feat(bookmark-places-page): [WIP] layout
    Related to: #80, #81

---
+++ src/views/MyPage/Bookmark/ui/TabBar.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+const TabBar: React.FC = () => {
+  return <></>;
+};
+
+export default TabBar;
---
+++ src/views/MyPage/Bookmark/ui/index.tsx
@@ -0,0 +1,22 @@
+import Header from "@/widgets/Layout/ui/Header";
+
+import BackButton from "@/shared/ui/BackButton";
+
+import { IconArrowLeft24 } from "public/icons";
+
+import styles from "./BookmarkPage.module.scss";
+
+const BookmarkPage: React.FC = () => {
+  return (
+    <main className={styles.wrapper}>
+      <Header className={styles.header}>
+        <BackButton className={styles.backButton}>
+          <IconArrowLeft24 />
+        </BackButton>
+        <h2 className={styles.title}>저장한 장소/코스</h2>
+      </Header>
+    </main>
+  );
+};
+
+export default BookmarkPage;
