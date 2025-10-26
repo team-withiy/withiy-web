63ce0a1b670361159549391d9c80c70c77b8ff90
rldnd <gi981226@gmail.com>
2025-10-26 23:49:58 +0900

    feat(place-review-page): apply infiniteScroll
    Related to: #86

--- src/views/Place/Review/ui/ReviewList.tsx
+++ src/views/Place/Review/ui/ReviewList.tsx
@@ -2,6 +2,8 @@
 
 import { useParams } from "next/navigation";
 
+import { range } from "lodash-es";
+
 import { placeQueries } from "@/entities/place/api/place.queries";
 import ReviewItem from "@/entities/review/ui/ReviewItem/ReviewItem.client";
 
@@ -16,21 +18,36 @@ const ReviewList: React.FC = () => {
   const { placeId } = useParams<{ placeId: string }>();
 
   return (
-    <InfiniteScroll.Suspense.Cursor.Vertical.Boundary
-      query={placeQueries.paginateReviews({ placeId: Number(placeId), sortBy: "latest" })}
-    >
-      {({ data, ...queryInfo }) => (
-        <div className={styles.wrapper}>
-          <ListTopSection total={data.meta.total} />
-          <InfiniteScroll.Suspense.Cursor.Vertical.Container data={data} elementType="ul" {...queryInfo}>
-            {data.data.map((review) => (
-              <ReviewItem review={review} className={styles.reviewItem} key={review.reviewId} />
-            ))}
-          </InfiniteScroll.Suspense.Cursor.Vertical.Container>
-        </div>
-      )}
-    </InfiniteScroll.Suspense.Cursor.Vertical.Boundary>
+    <>
+      <InfiniteScroll.Suspense.Cursor.Vertical.Boundary
+        query={placeQueries.paginateReviews({ placeId: Number(placeId), sortBy: "latest" })}
+      >
+        {({ data, ...queryInfo }) => (
+          <div className={styles.wrapper}>
+            <ListTopSection total={data.meta.total} />
+            <InfiniteScroll.Suspense.Cursor.Vertical.Container
+              isElementRoot
+              data={data}
+              elementType="ul"
+              loadingElements={range(10).map((value) => (
+                <ReviewItem.Loading className={styles.reviewItem} key={value} />
+              ))}
+              className={styles.list}
+              {...queryInfo}
+            >
+              {data.data.map((review) => (
+                <li key={review.reviewId} className={styles.reviewItem}>
+                  <ReviewItem review={review} />
+                </li>
+              ))}
+            </InfiniteScroll.Suspense.Cursor.Vertical.Container>
+          </div>
+        )}
+      </InfiniteScroll.Suspense.Cursor.Vertical.Boundary>
+    </>
   );
 };
 
-export default Suspense.with(ReviewList, { fallback: <>LOADING</> });
+export default Suspense.with(ReviewList, {
+  fallback: range(10).map((value) => <ReviewItem.Loading key={value} className={styles.reviewItem} />),
+});
