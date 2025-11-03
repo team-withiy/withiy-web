32edd0f7fc808d3f362433f88b828cb0d8c76deb
rldnd <gi981226@gmail.com>
2025-11-03 19:50:13 +0900

    feat(place-photo-list-page): photo list markup
    Related to: #88

--- src/views/Place/Photo/ui/PhotoList.tsx
+++ src/views/Place/Photo/ui/PhotoList.tsx
@@ -2,8 +2,12 @@
 
 import { useParams } from "next/navigation";
 
+import { range } from "lodash-es";
+import Skeleton from "react-loading-skeleton";
+
 import { placeQueries } from "@/entities/place/api/place.queries";
 
+import FallbackHandlerImage from "@/shared/ui/Image/FallbackHandlerImage";
 import { InfiniteScroll } from "@/shared/ui/InfiniteScroll";
 import Suspense from "@/shared/ui/Suspense";
 
@@ -14,17 +18,48 @@ const PhotoList: React.FC = () => {
 
   return (
     <InfiniteScroll.Suspense.Cursor.Vertical.Boundary query={placeQueries.paginatePhotos(Number(placeId))}>
-      {({ data, ...queryInfo }) => (
-        <InfiniteScroll.Suspense.Cursor.Vertical.Container data={data} {...queryInfo} elementType="ul" isElementRoot>
-          {data.data.map((photo) => (
-            <li className={styles.item} key={photo.photoId}>
-              <img src={photo.imageUrl} alt={`Photo ${photo.photoId}`} className={styles.image} />
-            </li>
-          ))}
+      {({ data, isFetchingNextPage, isFetchingPreviousPage, ...queryInfo }) => (
+        <InfiniteScroll.Suspense.Cursor.Vertical.Container
+          className={styles.wrapper}
+          data={data}
+          isFetchingNextPage={isFetchingNextPage}
+          isFetchingPreviousPage={isFetchingPreviousPage}
+          {...queryInfo}
+          elementType="div"
+          isElementRoot
+        >
+          <ul className={styles.list}>
+            {isFetchingPreviousPage &&
+              range(10).map((i) => <Skeleton key={i} containerClassName={styles.loadingItem} />)}
+            {data.data.map((photo) => (
+              <li className={styles.item} key={photo.photoId}>
+                <div className={styles.imageWrapper}>
+                  <FallbackHandlerImage
+                    src={photo.imageUrl}
+                    fill
+                    alt={`Photo ${photo.photoId}`}
+                    className={styles.image}
+                    fallbackSrc="/images/fallback.png"
+                  />
+                </div>
+              </li>
+            ))}
+            {isFetchingNextPage && range(10).map((i) => <Skeleton key={i} containerClassName={styles.loadingItem} />)}
+          </ul>
         </InfiniteScroll.Suspense.Cursor.Vertical.Container>
       )}
     </InfiniteScroll.Suspense.Cursor.Vertical.Boundary>
   );
 };
 
-export default Suspense.with(PhotoList, { fallback: <>LOADING</> });
+export default Suspense.with(PhotoList, {
+  fallback: (
+    <div className={styles.wrapper}>
+      <ul className={styles.list}>
+        {range(10).map((i) => (
+          <Skeleton key={i} containerClassName={styles.loadingItem} />
+        ))}
+      </ul>
+    </div>
+  ),
+});
